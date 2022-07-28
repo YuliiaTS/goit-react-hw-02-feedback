08d64936b80e22a60dd48c1c18db2cc36b8b149e
Yuliia Kovalchuk <iuliia1702@gmail.com>
2022-07-29 00:34:43 +0300

    рахує total, feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,71 @@ import style from '../components/App.module.css';
 
 class App extends Component {
   state = {
-     good: 0,
-     neutral: 0,
-     bad: 0,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
-  render () {
+
+  handelIncrement = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  handelIncrement2 = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  handelIncrement3 = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, bad, neutral } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    if (!good) {
+      return 0;
+    }
+    const total = good + neutral + bad;
+    const feedback = (good * 100) / total;
+    return Math.round(feedback);
+  };
+
+  render() {
     console.log(this.state);
     const { good, neutral, bad } = this.state;
     return (
       <div className={style.main}>
-        <Statistics good={good} neutral={neutral} bad={bad} />
+        <h2>Please leave feedback</h2>
+        <div style={{ display: 'inline-flex' }}>
+          <button type="button" name="good" onClick={this.handelIncrement}>
+            Good
+          </button>
+          <button type="button" name="neutral" onClick={this.handelIncrement2}>
+            Neutral
+          </button>
+          <button type="button" name="bad" onClick={this.handelIncrement3}>
+            Bad
+          </button>
+        </div>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback()}
+          positivePercentage={this.countPositiveFeedbackPercentage()}
+        />
       </div>
     );
-  }  
+  }
 }
 
 export default App;
